a91b7d9a7ff356fe6c6d4b26d504a8add766683c
Ryan <ryanpburnett@yahoo.com>
2021-01-23 11:29:15 -0600

    character route name function

--- server.js
+++ server.js
@@ -38,6 +38,14 @@ app.get('/api/characters', (req, res) => {
     res.json(characters)
 })
 // /api/characters/:routeName
+app.get('/api/characters/:routeName', (req, res) => {
+    console.log(req.params)
+    const targetCharacter = req.params.routeName
+    const character = characters.find(character => {
+        return character.routeName === targetCharacter
+    })
+    res.json(character)
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
