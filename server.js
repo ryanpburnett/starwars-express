4b090cb71acffae4b7f55fba40cc4937333e2863
Ryan <ryanpburnett@yahoo.com>
2021-03-26 17:25:12 -0500

    terrible commit message

--- server.js
+++ server.js
@@ -40,6 +40,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
+app.get('/add', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/add.html'))
+})
+
 /**
  * API ROUTES
  */
